8eb6ab38dcc7baf77c6a4b3fccc209b109728f26
Krushna <krishnaydv35@gmail.com>
2025-03-26 20:25:36 +0530

    corrected import

--- src/main/java/com/main/config/AppConfig.java
+++ src/main/java/com/main/config/AppConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;  // ✅ Correct Import
 
 import jakarta.servlet.http.HttpServletRequest;
 
